f61a6127c31d5852716a2677ca51c8341331bcc6
Enoch <enoch.shumway@icloud.com>
2025-04-05 15:35:43 -0600

    Standardized the output of serial and threaded runs

--- serial/main.cpp
+++ serial/main.cpp
@@ -9,13 +9,16 @@ int main(int argc, char* argv[]) {
     if (argc < 5) {
 		std::cerr << "Missing Arguments. Format in the following order: <input_filename> <output_filename> <height> <width>" << std::endl;
 		return 1;
-	}
+    }
 
     // Reading in parameters
     const char* input_filename = argv[1];
     const char* output_filename = argv[2];
     int height = std::stoi(argv[3]);
 	int width = std::stoi(argv[4]);
+
+    // Printing run info for logging purposes
+    printf("\nRunning a serial program with data size %d x %d.\n", height, width);
 
     // Initializing input and output memory space
 	std::vector<uint16_t> altitude_data(height * width);
@@ -34,31 +37,31 @@ int main(int argc, char* argv[]) {
     // Begining the search for visibility
     auto start_time = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < height; i += 1) {
-        auto start_time_2 = std::chrono::high_resolution_clock::now();
+        // auto start_time_2 = std::chrono::high_resolution_clock::now();
         for (int j = 0; j < width; j += 1) {
             visible_points(altitude_data, visibility_data, height, width, i, j);
-            std::cout << ".";
-            std::cout.flush();
+            // std::cout << ".";
+            // std::cout.flush();
         }
-        auto end_time_2 = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> duration_2 = end_time_2 - start_time_2;
-        std::cout << "Time taken for row " << i << ": " << duration_2.count() << " seconds" << std::endl;
+        // auto end_time_2 = std::chrono::high_resolution_clock::now();
+        // std::chrono::duration<double> duration_2 = end_time_2 - start_time_2;
+        // std::cout << "Time taken for row " << i << ": " << duration_2.count() << " seconds" << std::endl;
     }
     auto end_time = std::chrono::high_resolution_clock::now();
 
     
     // Printing out time data
     std::chrono::duration<double> duration = end_time - start_time;
-    std::cout << "Time taken for visible_points: " << duration.count() << " seconds" << std::endl;
+    std::cout << "Total Time: " << duration.count() << " seconds" << std::endl;
 
-    // Printing out the visibility_data
-    for (int x = 0; x < 30; x++) {
-        for (int y = 0; y < 30; y++) {
-            std::cout << visibility_data.at(y + x * width) << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
+    // // Printing out the visibility_data
+    // for (int x = 0; x < 30; x++) {
+    //     for (int y = 0; y < 30; y++) {
+    //         std::cout << visibility_data.at(y + x * width) << " ";
+    //     }
+    //     std::cout << "\n";
+    // }
+    // std::cout << "\n";
 
 
     // Writing out visiblity data
