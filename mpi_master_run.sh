fa8fda3c8e437f7c949743d41840b8d5ff82b3c2
Brandon Herrin <brandonherrin@hotmail.com>
2025-04-16 12:19:39 -0600

    Adding run script for MPI

---
+++ mpi_master_run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd ./tools
+
+# check if 6000x6000.raw exists
+if [ ! -f "6000x6000.raw" ]; then
+    echo "6000x6000.raw not found in tools directory... EXITING"
+    exit 1
+fi
+
+g++ resize_dataset.cpp -o resize
+g++ validate_datasets.cpp -o validate
+
+cd ../
+
+# -------------------- Run MPI implementation (Distributed Memory Parallel)
+
+# Insert calls here
+
+cd ./distributed-cpu
+# Parameter List <Data Size> <Number of Processes>
+sbatch run.sh 500 4
+sbatch run.sh 500 8
+sbatch run.sh 500 16
+
+sbatch run.sh 1000 4
+sbatch run.sh 1000 8
+sbatch run.sh 1000 16
+
+sbatch run.sh 2000 4
+sbatch run.sh 2000 8
+sbatch run.sh 2000 16
+
+cd ../
