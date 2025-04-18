8b5dcf7258d34e96acc4e70ae617226fc9474589
Brandon Herrin <brandonherrin@hotmail.com>
2025-04-18 12:20:40 -0600

    MPI Run Script for 2000x2000

--- mpi_master_run.sh
+++ mpi_master_run.sh
@@ -18,94 +18,20 @@ cd ../
 
 
 
-# ----------------------------------------------- Run Serial implementation
-
-cd ./serial
-
-# Parameter List <Data Size>
-sbatch serial_run.sh 500
-sbatch serial_run.sh 1000
-sbatch serial_run.sh 2000
-
-cd ../
-
-
-
-# -------------------- Run Threaded implementation (Shared Memory Parallel)
-
-cd ./shared-cpu
-# Parameter List <Data Size> <Number of Threads>
-sbatch threads_run.sh 500 4
-sbatch threads_run.sh 500 8
-sbatch threads_run.sh 500 16
-
-sbatch threads_run.sh 1000 4
-sbatch threads_run.sh 1000 8
-sbatch threads_run.sh 1000 16
-
-sbatch threads_run.sh 2000 4
-sbatch threads_run.sh 2000 8
-sbatch threads_run.sh 2000 16
-
-cd ../
-
-
-
 # -------------------- Run MPI implementation (Distributed Memory Parallel)
 
 cd ./distributed-cpu
 # Parameter List <Data Size> <Number of Processes>
-sbatch distributed_cpu_run.sh 500 4
-sbatch distributed_cpu_run.sh 500 8
-sbatch distributed_cpu_run.sh 500 16
+# sbatch distributed_cpu_run.sh 500 4
+# sbatch distributed_cpu_run.sh 500 8
+# sbatch distributed_cpu_run.sh 500 16
 
-sbatch distributed_cpu_run.sh 1000 4
-sbatch distributed_cpu_run.sh 1000 8
-sbatch distributed_cpu_run.sh 1000 16
+# sbatch distributed_cpu_run.sh 1000 4
+# sbatch distributed_cpu_run.sh 1000 8
+# sbatch distributed_cpu_run.sh 1000 16
 
-sbatch distributed_cpu_run.sh 2000 4
-sbatch distributed_cpu_run.sh 2000 8
-sbatch distributed_cpu_run.sh 2000 16
-
-cd ../
-
-
-
-# -------------------------------- Run GPU implementation (Non-Distributed)
-
-cd ./shared-gpu
-# Parameter List <Data Size> <Block Size>
-sbatch shared_gpu_run.sh 1000 5
-sbatch shared_gpu_run.sh 1000 15
-sbatch shared_gpu_run.sh 1000 32
-
-sbatch shared_gpu_run.sh 2000 5
-sbatch shared_gpu_run.sh 2000 15
-sbatch shared_gpu_run.sh 2000 32
-
-sbatch shared_gpu_run.sh 6000 5
-sbatch shared_gpu_run.sh 6000 15
-sbatch shared_gpu_run.sh 6000 32
-
-cd ../
-
-
-
-# ------------------------------------ Run GPU implementation (Distributed)
-
-cd ./distributed-gpu
-# Parameter List <Data Size> <Block Size> <Num Processes>
-sbatch --nodes=2 --ntasks=2 distributed_gpu_run.sh 1000 32 2
-sbatch --nodes=3 --ntasks=3 distributed_gpu_run.sh 1000 32 3
-sbatch --nodes=4 --ntasks=4 distributed_gpu_run.sh 1000 32 4
-
-sbatch --nodes=2 --ntasks=2 distributed_gpu_run.sh 2000 32 2
-sbatch --nodes=3 --ntasks=3 distributed_gpu_run.sh 2000 32 3
-sbatch --nodes=4 --ntasks=4 distributed_gpu_run.sh 2000 32 4
-
-sbatch --nodes=2 --ntasks=2 distributed_gpu_run.sh 6000 32 2
-sbatch --nodes=3 --ntasks=3 distributed_gpu_run.sh 6000 32 3
-sbatch --nodes=4 --ntasks=4 distributed_gpu_run.sh 6000 32 4
-
+sbatch --nodes=2distributed_cpu_run.sh 2000 4
+sbatch  --nodes=2distributed_cpu_run.sh 2000 8
+sbatch  --nodes=2distributed_cpu_run.sh 2000 16
 
 cd ../
